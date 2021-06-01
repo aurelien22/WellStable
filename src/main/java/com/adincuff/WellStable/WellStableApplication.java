909ff3dc75207c85a9c1b8679b1abb0db2d15e9f
aurelien22 <aurelien.dincuff@gmail.com>
2021-06-01 09:05:29 +0200

    Initial commit

---
+++ src/main/java/com/adincuff/WellStable/WellStableApplication.java
@@ -0,0 +1,13 @@
+package com.adincuff.WellStable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellStableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellStableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adincuff/WellStable/WellStableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adincuff.WellStable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellStableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
